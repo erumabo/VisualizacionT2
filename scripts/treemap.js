171dbde3384def17ebcf5ef53533fc70dab4fb05
mabo <mabo.daniel@gmail.com>
2019-09-23 01:39:32 -0600

    Interaccion con el treemap Carga de datos desde un raw de git Pendientes=>   Que al tocar una hoja genere el graf de barras   Limpieza general del codigo   Que se vea bonito

--- scripts/treemap.js
+++ scripts/treemap.js
@@ -1,35 +1,50 @@
-let root;
+let root,selectedNode;
 let width = 600, height=600;
 
-function archivo(event){
-  /* Todo inicia aqui, cuando se carga un archivo 
-   *
-   *
-   */
-  let reader = new FileReader();
-  reader.onload = ()=>{
-    let text = reader.result;
+function creaArbol(data){
     root = d3.stratify()
-            .id(d=>d.Subcategoria)
-            .parentId(d=>d.Categoria)
-      (d3.csvParse(text));
-    root
-      .sum(d=>d.Valor);
+      .id(d=>d.Subcategoria)
+      .parentId(d=>d.Categoria)
+        (data);
+    root.each(n=>n.value=+n.data.Valor);
+    root.sort((a,b)=>b.value-a.value);
     d3.treemap()
-        .size([width,height])
-        .padding(2)
-        .round(true)
-      (root);
-    setTimeout(grafica,100); /*Makes function call async*/
-  };
+      .size([width,height])
+      .paddingInner(4)
+      .paddingOuter(4)
+      .round(true)
+        (root);
+    selectedNode=root;
+    grafica();
+}
+
+function archivo(event){
+  let reader = new FileReader();
+  reader.onload = ()=>creaArbol(d3.csvParse(reader.result));
   reader.readAsText(event.target.files[0]);
 }
 
-let level = 0;
+function cambioSelected(nodo){
+  root.each(n=>{
+    if(n.id===nodo)
+      if(n.height===0) console.log(n);
+      else selectedNode=n;
+  });
+  grafica();
+}
+
 function grafica(){
+  if(!selectedNode) selectedNode=root;
+  /**estas lineas**/
+  let lista = new Set(selectedNode.ancestors());
+  selectedNode.ancestors().forEach(n=>{
+    if(n.children) n.children.forEach(p=>lista.add(p));
+  });
+  lista = Array.from(lista).sort((a,b)=>b.height-a.height);
+  /**No me gustan ni un pelo**/
   let canvas = d3.select('#treemap');
   canvas.selectAll('.node')
-    .data(root.descendants())
+    .data(lista)
       .join('rect')
         .classed('node',true)
         .style('cursor','pointer')
@@ -37,15 +52,13 @@ function grafica(){
         .attr('y',d=>d.y0)
         .attr('width',d=>(d.x1-d.x0))
         .attr('height',d=>(d.y1-d.y0))
-        .attr('fill',d=>`rgb(${(255/3)*d.depth},0,0)`)
-        .attr('onclick',(d,i)=>`console.log(${d.x0},${d.y0})`);
+        .attr('fill',d=>`rgb(${(255/3)*d.depth},0,0)`) //cambiar la escala de colores
+        .attr('title',d=>d.data.Subcategoria)
+        .attr('onclick',(d,i)=>`cambioSelected("${d.id}")`);
+  $('.node').tooltip({'container':'body'});
 }
 
 $(document).ready(()=>{
-  root = d3.stratify()
-    .id(d=>d.Subcategoria)
-    .parentId(d=>d.Categoria)
-    (d3.cvs())
   d3.select('#treemap')
       .attr('width',width+'px')
       .attr('height',height+'px')
@@ -55,5 +68,7 @@ $(document).ready(()=>{
         .attr('width','100%')
         .attr('height','100%')
         .attr('fill','rgb(206,206,206)'); //rect para ref visual del fondo
+  
+  d3.csv("https://raw.githubusercontent.com/erumabo/VisualizacionT2/master/data.csv").then(creaArbol); 
   console.log('ready');
 });
